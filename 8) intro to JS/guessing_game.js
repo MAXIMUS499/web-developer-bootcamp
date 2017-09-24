b08e31c988553a71cec8ad972c1a7f199df07a0c
maximus <maximus4999@gmail.com>
2017-09-24 13:19:40 +0300

    add some conditionals

--- 8) intro to JS/guessing_game.js
+++ 8) intro to JS/guessing_game.js
@@ -1,3 +1,31 @@
+//using a loop
+
+// Create secretNumber
+var secretNumber = 4;
+
+
+while(Number(number) !== secretNumber) {
+
+	// Ask user for a guess
+
+	var number = prompt("Guess a number!");
+
+	// Check if it is right
+	
+	if(Number(number) === secretNumber) {
+	alert("Wow! You've guessed the right number");
+	}
+	else if( number === "" || isNaN(number) ) {
+		alert("that is not a number you've entered!");
+	}
+	else if(number < secretNumber) {
+		alert("Too low! Try again.");
+	}
+	else if(number > secretNumber) {
+		alert("Too high! Try again.");
+	}
+}
+
 // var secretNumber = 4;
 
 // var number = prompt("Guess a number!");
@@ -25,23 +53,3 @@
 // else if (number == 7) {
 // 	alert("Wow! You've guessed the right number");
 // }
-
-
-var secretNumber = 4;
-var number = prompt("Guess a number!");
-
-
-while(Number(number) !== secretNumber) {
-	var number = prompt("Guess a number!");
-	if(Number(number) === secretNumber) {
-	alert("Wow! You've guessed the right number");
-	}
-	else if(number < secretNumber) {
-		alert("Too low! Try again.");
-	}
-
-	else if(number > secretNumber) {
-		alert("Too high! Try again.");
-	}
-
-}
