d7c71820a9b8bd1162f71018d3f6e9c2a6e452a4
maximus <maximus4999@gmail.com>
2017-10-04 13:30:28 +0300

    add the delete feature

--- 11) Javascript Basics Arrays/todolist.js
+++ 11) Javascript Basics Arrays/todolist.js
@@ -10,9 +10,27 @@ while(input !== "quit") {
 		var newTodo = prompt("Enter a new todo");
 		//add to todos array
 		todos.push(newTodo);
+		console.log(newTodo + " added to the list")
 	}
 	else if(input === "list") {
-		console.log(todos);
+		// console.log(todos);
+		console.log("************");
+		for(var i = 0; i < todos.length; i++) {
+			console.log(i + ": " + todos[i]);
+		}
+		console.log("************");
+	}
+	else if(input === "delete") {
+		//ask for index of todo to be deleleted
+		var deleteIndex = prompt("Enter index of todo to delete");
+		//delete that todo
+		if(deleteIndex > todos.length) {
+			console.log("You've entered the wrong index");
+		}
+		else {
+			todos.splice(deleteIndex, 1);
+			console.log("The todo was deleted");
+		}
 	}
 	//ask again for new input
 	input = prompt("What would you like to do?");
