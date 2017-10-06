ea8691d18976fcb2f9d9f1a6e2da53c683c7fecb
maximus <maximus4999@gmail.com>
2017-10-06 21:37:15 +0300

    finished section 13 - DOM manipulation

---
+++ Section 13 - DOM Manipulation/selectorExercise.js
@@ -0,0 +1,25 @@
+//MY SOLUTION
+
+var pSelector1 = document.querySelector("p");
+pSelector1;
+
+var pSelector2 = document.getElementById("first");
+pSelector2;
+
+var pSelector3 = document.getElementsByClassName("special")[0];
+pSelector3;
+
+var pSelector4 = document.getElementsByTagName("p")[0];
+pSelector4;
+
+// TEACHER'S SOLUTION
+
+document.getElementById("first");
+document.querySelector("#first");
+document.getElementsByClassName("special")[0];
+document.getElementsByTagName("p")[0];
+document.querySelector(".special");
+document.querySelectorAll(".special")[0];
+document.querySelector("p");
+document.querySelectorAll("p")[0];
+document.querySelector("h1 + p");
