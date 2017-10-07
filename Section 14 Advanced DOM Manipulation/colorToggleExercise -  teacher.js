746375fc05dd648e6f2a2f09367f7a448ee2af23
maximus <maximus4999@gmail.com>
2017-10-07 21:27:06 +0300

    teacher's solution is much better than mine

---
+++ Section 14 Advanced DOM Manipulation/colorToggleExercise -  teacher.js
@@ -0,0 +1,25 @@
+var button = document.querySelector("button");
+
+button.addEventListener("click", function() {
+	document.body.classList.toggle("purple");
+});
+
+// VERSION 1
+// var isPurple = false;
+	//if white
+		//make it purple
+	//else
+		//make it white
+
+// button.addEventListener("click", function() {
+// 	if(isPurple) {
+// 		document.body.style.background = "white";
+		
+// 	}
+// 	else {
+// 		document.body.style.background = "purple";
+		
+// 	}
+// 	isPurple = !isPurple;
+// });
+
