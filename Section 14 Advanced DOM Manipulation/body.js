5b933b54c960104cc9a91f90dfa7943912f8a146
maximus <maximus4999@gmail.com>
2017-10-07 20:47:04 +0300

    Succesfully finished the toggle color task - it was hard though))

---
+++ Section 14 Advanced DOM Manipulation/body.js
@@ -0,0 +1,33 @@
+// "use strict";
+
+// var body = document.querySelector("body");
+// var isBlue = false;
+
+// setInterval(function(){
+// 	if (isBlue) {
+// 		body.style.background = "#3498db";
+// 	}
+// 	else {
+// 		body.style.background = "#3498db";
+// 	}
+// 	isBlue = !isBlue;
+// }, 1000);
+
+// for (var i = 0; i < links.length; i++) {
+// console.log(links[i].textContent);
+// }
+
+
+// SECTION 14
+
+// var h1 = document.querySelector("h1");
+// h1.addEventListener("click", function(){
+//  alert("h1 was clicked!");
+// })
+// h1.addEventListener("click", function(){
+//  h1.style.background = "green";
+// })
+
+// document.querySelector("ul").addEventListener("click", function(){
+//  console.log("YOU CLICKED THE UL!");
+// });
