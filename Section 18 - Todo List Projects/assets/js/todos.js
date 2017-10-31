14b844a303b51fff5e39f92b03b193053a5ece64
MAXIMUS <maximus4999@gmail.com>
2017-10-31 13:44:46 +0200

    finished with the todo list

--- Section 18 - Todo List Projects/assets/js/todos.js
+++ Section 18 - Todo List Projects/assets/js/todos.js
@@ -1,14 +1,34 @@
 // Check Out Specific Todos By Clicking
-$("li").click(function(){
+//in the code below we added listener to the entire
+//ul parent, which means that all future li
+//will be proccessed through the on.click 
+//listener
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
 //Click on X to delete Todos
-$("span").click(function(event){
+$("ul").on('click', "span", function(event){
 	$(this).parent().fadeOut("slow", function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
-
 });
 
+
+//add a new todo
+$("input[type='text']").keypress(function(event){
+	if (event.which === 13) {
+		//grabing new todo text from input
+		var todoText = $(this).val();
+		//clear out the input
+		$(this).val("");
+		//create a bew li and add to ul.
+		//also - create this span which should delete the Todo
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+	}
+});
+
+$(".fa-plus").click(function() {
+	$("input[type='text']").fadeToggle();
+})
