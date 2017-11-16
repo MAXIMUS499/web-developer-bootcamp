0b216ec0d0a3d1228b94d5b98556697336c37287
MAXIMUS <maximus4999@gmail.com>
2017-11-16 18:37:54 +0200

    204 YelpCamp Styling Campgrounds

--- Section 25 - YelpCamp Basics/app.js
+++ Section 25 - YelpCamp Basics/app.js
@@ -1,21 +1,41 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var campgrounds = [
+		{name: "Salmon Creek", image: "https://farm4.staticflickr.com/3832/9603531635_e348167e39.jpg"},
+		{name: "Granite Hill", image: "https://farm3.staticflickr.com/2931/14128269785_f27fb630f3.jpg"},
+		{name: "Mountain Goat's Rest", image: "https://farm9.staticflickr.com/8358/8444469474_8f4b935818.jpg"},
+		{name: "Salmon Creek", image: "https://farm4.staticflickr.com/3832/9603531635_e348167e39.jpg"},
+		{name: "Granite Hill", image: "https://farm3.staticflickr.com/2931/14128269785_f27fb630f3.jpg"},
+		{name: "Mountain Goat's Rest", image: "https://farm9.staticflickr.com/8358/8444469474_8f4b935818.jpg"}
+];
 
 app.get("/", function(req, res){
 	res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-	var campgrounds = [
-		{name: "Salmon Creek", image: "https://farm4.staticflickr.com/3832/9603531635_e348167e39.jpg"},
-		{name: "Granite Hill", image: "https://farm3.staticflickr.com/2931/14128269785_f27fb630f3.jpg"},
-		{name: "Mountain Goat's Rest", image: "https://farm9.staticflickr.com/8358/8444469474_8f4b935818.jpg"}
-	]
+	
 
 	res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", function(req, res){
+	//get data from our form and add to campground array
+	var name = req.body.name;
+	var image = req.body.image;
+	var newCampground = {name: name, image: image}
+	campgrounds.push(newCampground);
+	//redirect back to campgrounds 	page
+	res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+	res.render("new.ejs");
 });
 
 app.listen(3000, () => console.log("The app is listening on port 3000!"))	
--- Section 25 - YelpCamp Basics/readme.md
+++ Section 25 - YelpCamp Basics/readme.md
@@ -1,4 +1,4 @@
-#YelpCamp
+#1)YelpCamp
 
 * Add Landing Pagee
 * Add Campgrounds Page
@@ -7,3 +7,19 @@ Each Campground has:
 
 	* Name
 	*Image
+
+#2)Layout and Basic Styling
+* Create our header and footer
+* Add in Bootstrap
+
+#3)Creating new Campgrounds
+* Setup new campgroung POST route
+* Add in body-parser
+* Setup route to show form
+* Add basic unstyled form
+
+#4) Style the campground page
+* Add a better header/title
+	* use a jumbotron in bootstrap
+	* add a grid of small images
+* Make campgrounds display in a grid
