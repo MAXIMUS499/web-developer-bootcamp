75c49aa0d6579824f82265412c80e31158283f75
MAXIMUS <maximus4999@gmail.com>
2017-11-23 18:30:19 +0200

    add description to camps, make a camp to be stored in mongo db

--- Section 27 YelpCamp Data Persistance/v2/app.js
+++ Section 27 YelpCamp Data Persistance/v2/app.js
@@ -12,19 +12,22 @@ app.set("view engine", "ejs");
 //SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
 	name: String,
-	image: String
+	image: String,
+	description: String
 }); 
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create(
 // 	{
-// 		name: "Granite Hill",
-// 	  image: "https://farm3.staticflickr.com/2931/14128269785_f27fb630f3.jpg"
+// 		name: 			 "Granite Hill",
+// 	  image: 			 "https://farm3.staticflickr.com/2931/14128269785_f27fb630f3.jpg",
+// 		description: "This is a huge granite hill, no bathrooms. No water. Beautiful granite"
 // 	},
 // 	{
-// 		name: "Maximus Glory Road",
-// 		image: "http://www.planetware.com/photos-large/USNH/usa-new-hampshire-campgrounds-franconia-notch.jpg"
+// 		name: 			 "Maximus Glory Road",
+// 		image: 			 "http://www.planetware.com/photos-large/USNH/usa-new-hampshire-campgrounds-franconia-notch.jpg",
+// 		description: "This a well-known road!!"
 // 	}, 
 // 	function(err, campground){
 // 		 	if(err) {
@@ -41,22 +44,26 @@ app.get("/", function(req, res){
 	res.render("landing");
 });
 
+//INDEX - show all Campgrounds
 app.get("/campgrounds", function(req, res){
 	//Get all camps from DB
 	Campground.find({}, function(err, allCampgrounds){
 		if (err) {
 		  console.log(err);
 		} else {
-			 res.render("campgrounds", {campgrounds: allCampgrounds});
+			 res.render("index", {campgrounds: allCampgrounds});
 		}
 	});
 });
 
+
+//CREATE - add new campground to DB
 app.post("/campgrounds", function(req, res){
 	//get data from our form and add to campground array
-	var name = req.body.name;
+	var name 	= req.body.name;
 	var image = req.body.image;
-	var newCampground = {name: name, image: image}
+	var desc  = req.body.description;
+	var newCampground = {name: name, image: image, description: desc}
 	//Create a new campground and save to DB
 	Campground.create(newCampground, function(err, newlyCreated){
 		if (err) {
@@ -68,8 +75,26 @@ app.post("/campgrounds", function(req, res){
 	res.redirect("/campgrounds");
 });
 
+//NEW - show form to create  new campground
 app.get("/campgrounds/new", function(req, res){
 	res.render("new.ejs");
+});
+
+//SHOW -show more info about one campground
+app.get("/campgrounds/:id", function(req, res){
+	//find the campground with provided ID
+	//Campground.FindById(id, callbackFunction)
+	Campground.findById(req.params.id, function(err, foundCampground){
+		if(err){
+			console.log(err);
+		} else {
+			//render show template with that campground
+			res.render("show", {campground: foundCampground});
+		}
+	});
+	
+	
+	
 });
 
 app.listen(3000, () => console.log("The app is listening on port 3000!"))	
---
+++ Section 27 YelpCamp Data Persistance/v2/list of camps.js
@@ -0,0 +1,9 @@
+var campgrounds = [
+		
+		{name: "Granite Hill", 				image: "https://farm3.staticflickr.com/2931/14128269785_f27fb630f3.jpg"},
+		{name: "Mountain Goat's Rest",image: "https://farm9.staticflickr.com/8358/8444469474_8f4b935818.jpg"},
+		{name: "Salmon Creek", 				image: "https://farm4.staticflickr.com/3832/9603531635_e348167e39.jpg"},
+		{name: "Granite Hill", 				image: "https://farm3.staticflickr.com/2931/14128269785_f27fb630f3.jpg"},
+		{name: "Mountain Goat's Rest",image: "https://farm9.staticflickr.com/8358/8444469474_8f4b935818.jpg"},
+		{name: "Maximus Glory Road",  image: "http://www.planetware.com/photos-large/USNH/usa-new-hampshire-campgrounds-franconia-notch.jpg"}
+];
