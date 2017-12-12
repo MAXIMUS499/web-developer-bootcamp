3edfa9371a7c38049c43ff65e540c77f79b1e9b9
MAXIMUS <maximus4999@gmail.com>
2017-12-12 17:04:23 +0200

    cover INDEX, NEW, CREATE; a little of Semantic UI

--- Section 28 RESTful Routing/RESTfulBlogApp/app.js
+++ Section 28 RESTful Routing/RESTfulBlogApp/app.js
@@ -31,6 +31,7 @@ app.get("/", function(req, res){
 	res.redirect("/blogs");
 });
 
+// INDEX ROUTE
 app.get("/blogs", function(req, res){
 	Blog.find({}, function(err, blogs){
 		if(err)
@@ -40,5 +41,22 @@ app.get("/blogs", function(req, res){
 	});
 });
 
+// NEW ROUTE
+app.get("/blogs/new", function(req, res){
+	res.render("new");
+});
+// CREATE ROUTE
+app.post("/blogs", function(req, res){
+	//create blog
+	Blog.create(req.body.blog, function(err, newBlog){
+		if (err)
+			res.render("new");
+		//then, redirect to the index
+		else
+			res.redirect("/blogs");
+	});
+	
+	
+});
 
 app.listen(3000, () => console.log("The app is listening on port 3000"));
