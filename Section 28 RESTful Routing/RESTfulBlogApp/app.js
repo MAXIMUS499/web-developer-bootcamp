88515e9c7732dd705d294226a5edff09020f80c8
MAXIMUS499 <maximus4999@gmail.com>
2017-12-18 17:17:20 +0200

    edit and update part

--- Section 28 RESTful Routing/RESTfulBlogApp/app.js
+++ Section 28 RESTful Routing/RESTfulBlogApp/app.js
@@ -1,16 +1,18 @@
 //1) point all required things
-var bodyParser = require("body-parser"),
+var methodOverride = require("method-override"), 
+		bodyParser = require("body-parser"),
 	  mongoose   = require("mongoose"),
 	  express 	 = require("express"),
+	  moment 		 = require('moment'),
 	  app 			 = express();
-	  moment 		 = require('moment');
 
 //2) set up basic things //APP CONFIG
 mongoose.connect("mongodb://localhost/restful_blog_app");
-// in order to ger rid of .ejs we use this:
+// in order to get rid of .ejs we use this:
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 //3) MONGOOSE/MODEL CONFIG //create schema
 var blogSchema = new mongoose.Schema({
@@ -68,6 +70,28 @@ app.get('/blogs/:id', function(req, res) {
 			res.render("show", {blog: foundBlog})
 		}
 	});
+});
+
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err)
+			res.redirect("/blogs");
+		else {
+			res.render("edit", {blog: foundBlog});
+		}
+	});
+});
+
+//UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBLog){
+		if(err)
+			res.redirect("/blogs");
+		else {
+			res.redirect("/blogs/" + req.params.id);
+		}
+	})
 });
 
 app.listen(3000, () => console.log("The app is listening on port 3000"));
